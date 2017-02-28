b55d8c0565a6134764f75576a87671fb3212e05f
Moses Yeung <yo_yeung@yahoo.com.hk>
2017-02-28 17:51:48 +0800

    update docker config

--- pushECS.sh
+++ pushECS.sh
@@ -4,4 +4,4 @@ sudo chmod +x /usr/local/bin/ecs-cli
 ecs-cli --version
 
 ecs-cli configure --region $AWS_REGION --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECERT_KEY --cluster test-uat
-ecs-cli compose service --project-name gfg-pricing-engine up
+ecs-cli compose --project-name gfg-pricing-engine service up
