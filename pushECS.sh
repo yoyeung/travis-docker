71fd1e2f02f9bb71450bd8ee385230e317539812
Moses Yeung <yo_yeung@yahoo.com.hk>
2017-02-28 17:30:03 +0800

    update docker config

--- pushECS.sh
+++ pushECS.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-darwin-amd64-latest
 sudo chmod +x /usr/local/bin/ecs-cli
 ecs-cli --version
